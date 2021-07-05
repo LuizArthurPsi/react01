519fdb8e318cf01eacfe559f8ad84d279e07bad0
Luiz Arthur <luizarthurvm@gmail.com>
2021-07-05 15:34:37 -0300

    Exibindo issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 export default class Repository extends Component {
     // eslint-disable-next-line react/static-property-placement
@@ -47,7 +47,6 @@ export default class Repository extends Component {
     }
 
     render() {
-        // eslint-disable-next-line no-unused-vars
         const { repository, issues, loading } = this.state;
 
         if (loading) {
@@ -64,6 +63,27 @@ export default class Repository extends Component {
                     <h1>{repository.name}</h1>
                     <p>{repository.description}</p>
                 </Owner>
+                <IssueList>
+                    {issues.map(issue => (
+                        <li key={String(issue.id)}>
+                            <img
+                                src={issue.user.avatar_url}
+                                alt={issue.user.login}
+                            />
+                            <div>
+                                <strong>
+                                    <a href={issue.html_url}>{issue.title}</a>
+                                    {issue.labels.map(label => (
+                                        <span key={String(label.id)}>
+                                            {label.name}
+                                        </span>
+                                    ))}
+                                </strong>
+                                <p>{issue.user.login}</p>
+                            </div>
+                        </li>
+                    ))}
+                </IssueList>
             </Container>
         );
     }
